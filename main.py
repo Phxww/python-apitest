3df1599c0bf71326bc7a6aa8d4f192064d7201b6
phoenix.wang-at-716383075839 <phoenix.wang@ecloudvalley.com>
2021-03-07 15:55:58 +0800

    11. Get,Post method flask  - post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -21,6 +21,15 @@ def helloworld():
 @app.route('/users', methods=['GET'])
 def get_user():
     return jsonify(user_list)
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    user = request.get_json()
+    user_list.append(user)
+    return jsonify({
+        'message': 'user created'
+    })
 
 
 if __name__ == "__main__":
